d09dbbac40c1a5a79b1396eec57f9adea0321d6c
Jeanneclre <>
2023-08-16 11:25:39 -0400

    Cmake tuto

---
+++ cmake-3.27.2-tutorial-source/Step2/MathFunctions/MathFunctions.cxx
@@ -0,0 +1,19 @@
+#include "MathFunctions.h"
+
+// TODO 11: include cmath
+#include "cmath"
+
+// TODO 10: Wrap the mysqrt include in a precompiled ifdef based on USE_MYMATH
+// #include "mysqrt.h"
+
+
+namespace mathfunctions {
+double sqrt(double x)
+{
+  #ifdef USE_MYMATH
+    return detail::mysqrt(x);
+  #else
+    return std::sqrt(x);
+  #endif
+}
+}
